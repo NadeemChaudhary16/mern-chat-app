a0e6ef413db80594feecb48494fea7e19be9f747
NadeemChaudhary16 <nadeemchaudhary808@gmail.com>
2024-09-28 17:21:23 +0530

    change build to dist

--- server/index.js
+++ server/index.js
@@ -51,10 +51,10 @@ app.use("/api/message", MessageRoute);
 // --------------------------deployment------------------------------
 const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "../client/build")));
+  app.use(express.static(path.join(__dirname1, "../client/dist")));
 
   app.get("*", (req, res) =>
-      res.sendFile(path.resolve(__dirname1, "../client/build", "index.html"))
+      res.sendFile(path.resolve(__dirname1, "../client/dist", "index.html"))
   );
 }
 else {
